e38f5d9febd4a3f530db7c3183f602e9b981a925
urooj-tes <urooj.baig@targetdigitalventures.com>
2025-07-28 21:48:18 +0500

    Conflicts

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Sidebar from '../components/side-bar'; 
 import { Outlet } from 'react-router-dom';
@@ -19,25 +18,3 @@ const AppLayout = () => {
 };
 
 export default AppLayout;
-=======
-import React from 'react';
-import Sidebar from '../components/side-bar'; 
-import { Outlet } from 'react-router-dom';
-import Header from '../components/top-bar';
-
-const AppLayout = () => {
-  return (
-    <div>
-      <Header />
-      <div style={{ display: 'flex' }}>
-        <Sidebar />
-        <main style={{ padding: '1rem', flex: 1 }}>
-          <Outlet />
-        </main>
-      </div>
-    </div>
-  );
-};
-
-export default AppLayout;
->>>>>>> 204ba91b83a6982270dbd52da0ed502f56ee68e0
