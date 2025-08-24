a860fcc1e1bae1f8e18afc765751eb5f9be4549d
HuzaifaAawan <awanhuzaifa478@gmail.com>
2025-08-24 17:06:31 +0500

    font changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import AppRoutes from "./routes/AppRoutes";
+// index.js or App.js
+// import "@fontsource/inter/variable.css"; // optional: import weights you need
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         brandGreen: "#04544f", // custom color name
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // Inter font as default
       },
     },
   },
